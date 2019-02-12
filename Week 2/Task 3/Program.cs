f01f450db46129bbde737a73b9cd94b425d8124f
Ingkar1 <kumarovainkar@gmail.com>
2019-02-12 11:55:50 +0600

    Changed

--- Week 2/Task 3/Program.cs
+++ Week 2/Task 3/Program.cs
@@ -8,66 +8,15 @@ namespace Task_3
 {
     class Program
     {
-        /*static void Ex1()
-        {
-
-            FileInfo file = new FileInfo("‪C:/Users/ww/Desktop/cafe/cafe.jpg");
-            Console.WriteLine(file.Name);
-            Console.WriteLine(file.Directory);
-            Console.WriteLine(file.FullName);
-        }*/
-        /*static void Ex2()
-        {
-            DirectoryInfo directory = new DirectoryInfo("‪C:/Users/ww/Desktop/");
-            Console.WriteLine(directory.Name);
-            Console.WriteLine(directory.FullName);
-            Console.WriteLine(directory.Parent);
-        }*/
-        /*static void Ex3()
-        {
-            DirectoryInfo Directory = new DirectoryInfo(" "); 
-            FileInfo[] files = Directory.GetFiles();
-            foreach (FileInfo file in files)
-            {
-                Console.WriteLine(file.Name);
-            }
-        }
-         public static void PrintSpace(int level)
-            {
-            for (int i = 0; i < level; i++)
-                Console.WriteLine("  ");
-             }
-        
-          
        
-        public static void Ex5(DirectoryInfo directory,int level)
-        {   
-            FileInfo[] files = directory.GetFiles();
-            DirectoryInfo[] directories = directory.GetDirectories();
-            
-            
-            foreach(FileInfo file in files)
-            {
-                PrintSpace(level);
-                Console.WriteLine(file.Name);
-            }
-            foreach(DirectoryInfo d in directories)
-            {
-                PrintSpace(level);
-                Console.WriteLine(d.Name);
-                Ex5(d, level + 1);
-
-            }
-        }
-        */
-        static void PrintInfo(FileSystemInfo fsi,int k)
+        static void PrintInfo(FileSystemInfo f,int k)
             {
             string line=new string(' ',k);
-            line=line+fsi.Name;
+            line=line+f.Name;
             Console.WriteLine(line);
-            if(fsi.GetType() == typeof(DirectoryInfo))
+            if(f.GetType() == typeof(DirectoryInfo))
                 {
-                var items=(fsi as DirectoryInfo).GetFileSystemInfos();
+                var items=(f as DirectoryInfo).GetFileSystemInfos();
                 foreach(var i in items)
                     {
                     PrintInfo(i,k+4);
@@ -75,15 +24,13 @@ namespace Task_3
                 }
             }
            
-        private static void F()
-            {
-             DirectoryInfo dir = new DirectoryInfo(@"C:\Users\ww\photo");
-                PrintInfo(dir, 1);
-            }
+       
 
         static void Main(string[] args)
-        {
-          F();
+        { 
+            DirectoryInfo d = new DirectoryInfo("C:/Users/ww/photo");
+                PrintInfo(d, 1);
+          
             Console.ReadKey();
         }
         
