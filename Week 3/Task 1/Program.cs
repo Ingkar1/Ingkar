9884a541e2f70d8fbab4ff8711d41e5974a8dde8
Ingkar1 <kumarovainkar@gmail.com>
2019-02-14 00:55:52 +0600

    Week 3

--- Week 3/Task 1/Program.cs
+++ Week 3/Task 1/Program.cs
@@ -1,43 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-namespace Task_1
+
+namespace Ex1
 {
     class FarManager
     {
         public int cursor;
+        public string path;
         public int sz;
-        bool ok;
+        public bool ok;
+        DirectoryInfo directory = null;
+        FileSystemInfo currentFs = null;
 
         public FarManager()
         {
             cursor = 0;
+        }
+
+        public FarManager(string path)
+        {
+            this.path = path;
+            cursor = 0;
+            directory = new DirectoryInfo(path);
+            sz = directory.GetFileSystemInfos().Length;
             ok = true;
         }
-        public void Up()
-        {
-            cursor--;
-            if (cursor < 0)
-                cursor = sz - 1;
 
-        }
-        public void Down()
+        public void Color(FileSystemInfo fs, int index,int i)
         {
-            cursor++;
-            if (cursor == sz)
-                cursor = 0;
-        }
-        public void Color(FileSystemInfo fs,int index)
-        {
-            if (index == cursor)
+            if (cursor == index && cursor==i)
             {
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.ForegroundColor = ConsoleColor.White;
+                currentFs = fs;
             }
-            else if(fs.GetType()==typeof(DirectoryInfo))
+            else if (fs.GetType() == typeof(DirectoryInfo))
             {
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.White;
@@ -45,13 +42,138 @@ namespace Task_1
             else
             {
                 Console.BackgroundColor = ConsoleColor.Black;
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+        }
+
+        public void Show()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            directory = new DirectoryInfo(path);
+            FileSystemInfo[] fs = directory.GetFileSystemInfos(); 
+           
+            for (int i = 0, k = 0; i < fs.Length; i++)
+            {
+                if (ok == false && fs[i].Name[0] == '.')
+                {
+                    continue;
+                }
+             
+                
+                    
+                Color(fs[i], k,i);
+                Console.Write(i+1+". ");
+                Console.WriteLine(fs[i].Name);
+                k++;
+            }
+        }
+        public void Up()
+        {
+            cursor--;
+            if (cursor < 0)
+                cursor = sz - 1;
+        }
+        public void Down()
+        {
+            cursor++;
+            if (cursor == sz)
+                cursor = 0;
+        }
+        public void Opentxt(string path)
+            {
+            Console.Clear();
+            StreamReader sr=new StreamReader(path);
+            string s=sr.ReadToEnd();
+            Console.WriteLine(s);
+            ConsoleKeyInfo k1=Console.ReadKey();
+            if(k1.Key==ConsoleKey.Backspace)
+                {
+                sr.Close();
+                return;
+            }
+            else
+            Opentxt(path);
+            }
+        public void CalcSz()
+        {
+            directory = new DirectoryInfo(path);
+            FileSystemInfo[] fs = directory.GetFileSystemInfos();
+            sz = fs.Length;
+            if (ok == false)
+                for (int i = 0; i < fs.Length; i++)
+                    if (fs[i].Name[0] == '.')
+                        sz--;
+        }
+
+        public void Start()
+        {
+            ConsoleKeyInfo consoleKey = Console.ReadKey();
+            while (consoleKey.Key != ConsoleKey.Escape)
+            {
+                CalcSz();
+                Show();
+                consoleKey = Console.ReadKey();
+                if (consoleKey.Key == ConsoleKey.UpArrow)
+                    Up();
+                if (consoleKey.Key == ConsoleKey.DownArrow)
+                    Down();
+                if (consoleKey.Key == ConsoleKey.RightArrow)
+                {
+                    ok = false;
+                    cursor = 0;
+                }
+                if (consoleKey.Key == ConsoleKey.LeftArrow)
+                {
+                    cursor = 0;
+                    ok = true;
+                }
+                if (consoleKey.Key == ConsoleKey.Enter)
+                {
+                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                    {
+                        cursor = 0;
+                        path = currentFs.FullName;
+                    }
+                    else if(currentFs.Name.EndsWith(".txt"))
+                        Opentxt(currentFs.FullName);
+                    
+                }
+                if (consoleKey.Key == ConsoleKey.Backspace)
+                {
+                    cursor = 0;
+                    path = directory.Parent.FullName;
+                }
+                if(consoleKey.Key==ConsoleKey.Delete)
+                {           
+                           cursor=0;
+                           currentFs.Delete();
+                }
+                if(consoleKey.Key==ConsoleKey.N)
+                    {
+                    string a=Console.ReadLine();
+                    a=Path.Combine(directory.FullName,a);
+                    if(currentFs.GetType()== typeof(DirectoryInfo))
+                        Directory.Move(currentFs.FullName,a);
+                    else
+                        File.Move(currentFs.FullName,a);
+                    }
+
+                
+                }
             }
 
-        }
+
+    }
+
+
+    class Program
+    {
         static void Main(string[] args)
         {
-            
+            string path = "C:/History";
+            FarManager farManager = new FarManager(path);
+            farManager.Start();
         }
     }
 }
